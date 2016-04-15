7e1c9db769cc7c28bd81a2ab66d77c82d90fc3c5
LK <maxen940393@gmail.com>
2016-04-15 23:37:01 +0200

    LeftHand

---
+++ Assets/Scripts/LeftStickInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftStickInput : MonoBehaviour {
+
+    private Vector3 moveDirection = Vector3.zero;
+    public float moveSpeed;
+
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () 
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moveDirection =new Vector3( Input.GetAxis("Horizontal") * moveSpeed,0, Input.GetAxis("Vertical") * moveSpeed);
+
+        //moveDirection = transform.TransformDirection(moveDirection);
+        rb.AddForce(moveDirection,ForceMode.Force);
+
+        moveDirection = Vector3.zero;
+       // transform.Translate(transform.position + moveDirection);
+    }
+}
