a77394a3ace863f70947d5cd91ed0eb6ed7ffa47
Stureplan <linus.ax@gmail.com>
2016-04-16 00:23:09 +0200

    Input fix left/right

--- Assets/Scripts/RightStickInput.cs
+++ Assets/Scripts/RightStickInput.cs
@@ -13,19 +13,15 @@ public class RightStickInput : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-        
-        if (movement.x < 0.1f)
-        {
-            //movement.x = 0.0f;
-        }
 
-        if (movement.z < 0.1f)
-        {
-           // movement.z = 0.0f;
-        }
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("HorizontalRight"), 0.0f, Input.GetAxis("VerticalRight"));
 
         Debug.Log(movement);
         rb.velocity = new Vector3(movement.x, movement.y, movement.z);
-	}
+        movement = Vector3.zero;
+    }
 }
