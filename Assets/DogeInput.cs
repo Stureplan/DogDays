7893d9d99e875e87c73ef340786c29c9fec2cbe2
Stureplan <linus.ax@gmail.com>
2016-04-16 22:55:07 +0200

    scene

--- Assets/DogeInput.cs
+++ Assets/DogeInput.cs
@@ -3,18 +3,21 @@ using System.Collections;
 
 public class DogeInput : MonoBehaviour
 {
+    Vector3 def;
     Rigidbody rb;
     float speed = 10.0f;
 
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        def = transform.position;
 	}
 	
 	void Update ()
     {
+        LerpTowards(def);
 
-	}
+    }
 
     void FixedUpdate()
     {
@@ -23,5 +26,10 @@ public class DogeInput : MonoBehaviour
         rb.velocity = new Vector3(movement.x, movement.y, movement.z);
 
         movement = Vector3.zero;
+    }
+
+    void LerpTowards(Vector3 pos)
+    {
+        transform.position = Vector3.Lerp(transform.position, pos, Time.deltaTime * 2.0f);
     }
 }
