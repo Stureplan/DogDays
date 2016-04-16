5dc50b7f97803cec415fd6ea24cb9e5905bd407b
LK <maxen940393@gmail.com>
2016-04-16 22:51:58 +0200

    Timer

--- Assets/PyttInPanUI.cs
+++ Assets/PyttInPanUI.cs
@@ -17,6 +17,9 @@ public class PyttInPanUI : MonoBehaviour {
 
     public Text winner;
 
+    public Text startCountDownText;
+    int startCountDown;
+
     float timeDown;
 
     string origString;
@@ -27,6 +30,7 @@ public class PyttInPanUI : MonoBehaviour {
 
 	void Start ()
     {
+        startCountDown = 3;
         gameOver = false;
         winShow.SetActive(false);
         gamePan = GameObject.Find("pan_Left");
@@ -53,6 +57,19 @@ public class PyttInPanUI : MonoBehaviour {
         int intTime = (int)timeDown;
         timeCount.text = intTime.ToString();
 
+        if (startCountDown >= 0)
+        {
+            startCountDownText.text = startCountDown.ToString();
+            Time.timeScale = 0;
+            System.Threading.Thread.Sleep(1000);
+           
+            startCountDown -= 1;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            startCountDownText.text = "";
+        }
 
         counterOfPytt.text = origString + pan.GetNrOfFoods().ToString();
 
