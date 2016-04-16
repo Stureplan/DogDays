124a1ea03072deeca872700b47dcc7668a382b8c
LK <maxen940393@gmail.com>
2016-04-16 19:00:42 +0200

    Pytt In Pan UI

---
+++ Assets/PyttInPanUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+
+public class PyttInPanUI : MonoBehaviour {
+
+	// Use this for initialization
+
+    Pan pan;
+    GameObject gamePan;
+
+    public Text text;
+
+    string origString;
+	void Start ()
+    {
+        gamePan = GameObject.Find("pan_Left");
+
+        pan = gamePan.GetComponent<Pan>();
+
+        origString = "Pytt in Pan : ";
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        text.text = origString + pan.GetNrOfFoods().ToString();
+	}
+}
