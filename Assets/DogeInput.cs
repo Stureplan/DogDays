fc3096227a69a19e5acdfffbad109c0616badb02
Stureplan <linus.ax@gmail.com>
2016-04-16 20:23:31 +0200

    Cookiing

---
+++ Assets/DogeInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogeInput : MonoBehaviour
+{
+    Rigidbody rb;
+    float speed = 10.0f;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+    {
+
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("HorizontalRight2"), 0.0f, Input.GetAxis("VerticalRight2"));
+        movement *= speed;
+        rb.velocity = new Vector3(movement.x, movement.y, movement.z);
+
+        movement = Vector3.zero;
+    }
+}
