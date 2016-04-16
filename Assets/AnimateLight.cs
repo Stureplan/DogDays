2517f2ff73fd25f97d1b832194a1b5b735580350
Stureplan <linus.ax@gmail.com>
2016-04-17 01:35:51 +0200

    Your move

---
+++ Assets/AnimateLight.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
